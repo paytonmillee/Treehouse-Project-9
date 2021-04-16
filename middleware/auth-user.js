419a29fd38a6c97b5f852c94b1413622829517f6
paytonmillee <74552655+paytonmillee@users.noreply.github.com>
2021-04-16 08:28:57 -0500

    final

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -1,35 +1,31 @@
 const auth = require("basic-auth");
 const bcrypt = require("bcryptjs");
 const { Users } = require("../models");
-
 //Authenticating the User by email
-exports.authenticateUser = (req, res, next) => {
+exports.authenticateUser = async (req, res, next) => {
   let message;
   const credentials = auth(req);
-
+  console.log(credentials);
   if (credentials) {
-    Users.findOne({
+    let user = await Users.findOne({
       where: { emailAddress: credentials.name },
-    })
-      .then((userInfo) => {
-        console.log(userInfo.dataValues);
-        const authenticated = bcrypt.compare(
-          credentials.pass,
-          userInfo.password
-        );
-        ///if true authentication becomes successful
-        if (authenticated) {
-          console.log(`Authentication successful`);
-
-          req.currentUser = userInfo;
-          ///else username is not recognized or valid and user can not move on to authenticate their password
-        } else {
-          message = `Authentication failure for username: ${userInfo.email}`;
-        }
-      })
-      .catch((error) => {
-        message = `User not found for username: ${credentials.name}`;
-      });
+    });
+    if (user) {
+      const authenticated = await bcrypt.compare(
+        credentials.pass,
+        user.password
+      );
+      ///if true authentication becomes successful
+      if (authenticated) {
+        console.log(`Authentication successful for ${user.email}`);
+        req.currentUser = user;
+        ///else username is not recognized or valid and user can not move on to authenticate their password
+      } else {
+        message = `Authentication failure for username: ${user.email}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
   } else {
     message = "Auth header not found";
   }
